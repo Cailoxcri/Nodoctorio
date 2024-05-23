3640b2e14a8723d1b467c53e1d5819f4e5dbbe92
Cailox <camilocasas09@gmail.com>
2024-05-23 11:10:17 -0500

    Añadimos un espacio para que sea legible el raizinput

--- Main.py
+++ Main.py
@@ -4,10 +4,18 @@ import os
 
 Nombre_archivo= 'archivo.txt'
 
-os.chdir('/home/cailox/Documentos')
+# Obtener el directorio home del usuario
+home_dir = os.path.expanduser("~")
+raizinput =os.path.expanduser(
+    input("Ingrese la dirección de la carpeta enla que quiere iniciar el programa (ejemplo '~/Documentos') :")
+    )
+print(raizinput)
 
-carpetas = [carpeta for carpeta in os.listdir() if '.' not in carpeta]
 
-Nodos = [ nodo(os.getcwd(),carpeta) for carpeta in carpetas]
+
+
+carpetas = [carpeta for carpeta in os.listdir(raizinput) if os.path.isdir(raizinput + '/' + carpeta)]
+print(carpetas)
+Nodos = [ nodo(raizinput,carpeta) for carpeta in carpetas]
 
 [item.resumen(Nombre_archivo) for item in Nodos]
--- nodos.py
+++ nodos.py
@@ -1,15 +1,29 @@
 import os
+Nombre_archivo= 'archivo.txt'
 
+def salto(string):
+    with open('Cambio','+r') as linea:
+        salto = linea.read()
+    
+    return salto.replace('Remplazar',str(string))
 
 class nodo:
+    
+        
+
     def __init__(self, raiz, nombre):
         self.raiz = raiz
         self.nombre = nombre
         self.direccion = raiz+ '/' + self.nombre
-        self.contenido =os.listdir(self.nombre)
-
+        self.nivel = self.direccion.count('/')
+        self.contenido =os.listdir(self.direccion)
+        self.carpetas = [carpeta for carpeta in self.contenido if os.path.isdir(self.direccion +  '/' + carpeta)]
+        self.ramas = [nodo(self.direccion,carpeta) for carpeta in self.carpetas]
+        [item.resumen(Nombre_archivo) for item in self.ramas]
 
     def resumen (self, archivo):
-        with open(archivo, 'a') as archivo:
-            archivo.write(' Dirección de la carpeta '+self.direccion + '\n')
-            archivo.write(' Carpetas dentro '+ str(self.contenido) + '\n')
+        if self.carpetas != []:
+            with open(archivo, 'a') as archivo:
+                archivo.write(' Dirección de la carpeta '+self.direccion  + '\n' )
+                archivo.write(' Carpetas dentro '+ str(self.carpetas) + '\n')
+                archivo.write( salto(self.nivel))
